dcb5591b4c28de8ce665cbec9deb0cc946fbcf08
NickCWilkins <nickcwilkins@yahoo.com>
2019-05-05 21:07:27 -0400

    add Statements class for SQL stuff

---
+++ src/SQL/Statements.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQL;
+
+/**
+ *
+ * @author Direspark
+ */
+public class Statements {
+    public final static String GET_EMPLOYEES = "SELECT * FROM jdelar1db.Employee;";
+}
